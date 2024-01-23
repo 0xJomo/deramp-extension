6ba723bf8163c66c186f130a2d2e4f5e9391f1b5
rpm <ryan.macarthur@gmail.com>
2024-01-23 11:59:40 -0400

    style

--- src/popup/App.js
+++ src/popup/App.js
@@ -1,12 +1,14 @@
 import logo from 'url:../images/icon256.png';
-
+import shapes from 'url:../images/shapes.svg';
 
 export const App = () => {
     return (
         <>
-            <img src={logo} alt="Logo"/>
-            <h1>Deramp</h1>
+            <div>
+            <img src={shapes} className="absolute animate-blob2" />
+            <h1 className="text-7xl lg:text-9xl font-extrabold">DeRamp</h1>
             <div>v{browser.runtime.getManifest().version}</div>
+            </div>
         </>
     );
 };
