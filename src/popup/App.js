83a2eb24ad0537481ec5151665d8361c99bf3cfa
Charlie Chen <charlie@jomo.network>
2024-01-31 14:26:37 -0800

    Update names and descriptions to use Jomo Copilot name

--- src/popup/App.js
+++ src/popup/App.js
@@ -1,13 +1,12 @@
-import logo from 'url:../images/icon256.png';
 import shapes from 'url:../images/shapes.svg';
 
 export const App = () => {
     return (
         <>
             <div>
-            <img src={shapes} className="absolute animate-blob2" />
-            <h1 className="text-7xl lg:text-9xl font-extrabold">DeRamp</h1>
-            <div>v{browser.runtime.getManifest().version}</div>
+                <img src={shapes} className="absolute animate-blob2" />
+                <h1 className="text-7xl lg:text-9xl font-extrabold">Jomo Copilot</h1>
+                <div>v{browser.runtime.getManifest().version}</div>
             </div>
         </>
     );
